4bd6bb7d7fa91130fe3edbd6895940d6d35472dd
Raleigh Wayland <rtwayland@gmail.com>
2016-10-04 14:06:59 -0600

    Complete email form

--- email-form.php
+++ email-form.php
@@ -1,11 +1,10 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
 
-// require_once 'libs/PHPMailer/PHPMailerAutoload.php';
-
-
+require_once 'libs/PHPMailer/PHPMailerAutoload.php';
+require_once 'email-credentials.php';
 
 // Collect form inputs
 $firstName = $lastName = $from = $messageContent = $phone = '';
@@ -30,58 +29,43 @@ function test_input($data)
 }
 
 // Set up email info
-// $to = "amandasniderphotography@yahoo.com";
-// $to = "poochy45@aol.com";
-$to = "rtwayland@gmail.com";
+$to = "amandasniderphotography@yahoo.com";
 $subject = 'Photo Shoot Inquiry';
-$headers = 'From: amandasniderphotography.com' . "\r\n" .
-    'Reply-To: ' . $from . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
 $customerName = $firstName . " " . $lastName;
 
 if (isset($phone)) {
-  $message = $messageContent . "\n" . "—" . $customerName . "—" . $phone;
+  $message = $messageContent . "\r\n" . " - " . $customerName . " — " . $phone;
 } else {
-  $message = $messageContent . "\n" . "—" . $customerName;
+  $message = $messageContent . "\r\n" . " - " . $customerName;
 }
 
 $message = wordwrap($message, 70, "\r\n");
 
 
 
-// $mail = new PHPMailer;
-//
-// $mail->isSMTP();
-// //$mail->Host = 'smtp.mail.yahoo.com; smtp.gmail.com; smtp.aol.com';
-//
-// $mail->Host = 'smtp.aol.com';  // Specify main and backup SMTP servers
-// $mail->SMTPAuth = true;                               // Enable SMTP authentication
-// $mail->Username = '';                 // SMTP username
-// $mail->Password = '';                           // SMTP password
-// $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-// $mail->Port = 587;                                    // TCP port to connect to
-//
-// $mail->setFrom($from, $customerName);
-// $mail->addAddress($to, 'Amanda Snider');     // Add a recipient
-// $mail->addReplyTo($from, 'Information');
-//
-// $mail->Subject = 'Photo Shoot Inquiry';
-// $mail->Body    = $message;
+$mail = new PHPMailer;
+
+$mail->isSMTP();
+$mail->Host = 'smtp.mail.yahoo.com; smtp.gmail.com; smtp.aol.com';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                                             // Enable SMTP authentication
+$mail->Username = $username;                                        // SMTP username
+$mail->Password = $password;                                        // SMTP password
+$mail->SMTPSecure = 'tls';                                          // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 587;                                                  // TCP port to connect to
+
+$mail->setFrom($username);
+$mail->addAddress($to);     // Add a recipient
+$mail->addReplyTo($from);
+
+$mail->Subject = 'Photo Shoot Inquiry';
+$mail->Body    = $message;
 // $mail->AltBody = $message;
-//
-// if($mail->send()) {
-//     header("location: email-confirmation.html");
-// } else {
-//   echo 'Message could not be sent.';
-//   echo 'Mailer Error: ' . $mail->ErrorInfo;
-// }
 
+if($mail->send()) {
+    header("location: email-confirmation.html");
+} else {
+  header("location: email-failure.html");
+  // echo 'Mailer Error: ' . $mail->ErrorInfo;
+}
 
-
-
-
-mail($to, $subject, $message, $headers);
-
-header("location: email-confirmation.html");
-die("Page should have been redirected");
- ?>
+?>
