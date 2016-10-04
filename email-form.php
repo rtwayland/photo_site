15e667e1a2d7180e3b114114d03d50b016c7daba
Raleigh Wayland <rtwayland@gmail.com>
2016-10-04 13:08:59 -0600

    Adding variable for phone number

--- email-form.php
+++ email-form.php
@@ -8,13 +8,17 @@ error_reporting(E_ALL);
 
 
 // Collect form inputs
-$firstName = $lastName = $from = $messageContent = '';
+$firstName = $lastName = $from = $messageContent = $phone = '';
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   $firstName = test_input($_POST['first-name']);
   $lastName = test_input($_POST['last-name']);
   $from = test_input($_POST['email']);
   $messageContent = test_input($_POST['message']);
+
+  if (isset($_POST['phone'])) {
+    $phone = test_input($_POST['phone']);
+  }
 }
 
 function test_input($data)
@@ -35,16 +39,21 @@ $headers = 'From: amandasniderphotography.com' . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
 $customerName = $firstName . " " . $lastName;
 
-$message = $messageContent . "\n" . "—" . $customerName;
+if (isset($phone)) {
+  $message = $messageContent . "\n" . "—" . $customerName . "—" . $phone;
+} else {
+  $message = $messageContent . "\n" . "—" . $customerName;
+}
+
 $message = wordwrap($message, 70, "\r\n");
 
 
 
 // $mail = new PHPMailer;
-
+//
 // $mail->isSMTP();
-//$mail->Host = 'smtp.mail.yahoo.com; smtp.gmail.com; smtp.aol.com';
-
+// //$mail->Host = 'smtp.mail.yahoo.com; smtp.gmail.com; smtp.aol.com';
+//
 // $mail->Host = 'smtp.aol.com';  // Specify main and backup SMTP servers
 // $mail->SMTPAuth = true;                               // Enable SMTP authentication
 // $mail->Username = '';                 // SMTP username
@@ -70,9 +79,7 @@ $message = wordwrap($message, 70, "\r\n");
 
 
 
-// $mailheader = 'From: "' . $customerName . '" <' . $from . '>'  . "\r\n" .
-// 'X-Mailer: PHP v' . phpversion();
-//
+
 mail($to, $subject, $message, $headers);
 
 header("location: email-confirmation.html");
